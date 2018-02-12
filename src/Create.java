affc80031d924170e73c0b54be23acc40549bfd1
Fabio Ribeiro <fabio.ribeiro.neto@gmail.com>
2018-02-12 21:15:59 -0200

    add main menu

--- src/Create.java
+++ src/Create.java
@@ -10,26 +10,32 @@ import org.apache.jena.vocabulary.RDF;
 
 public class Create {
 	
+	//initializing RDF model and Input Scanner
 	Model _model = ModelFactory.createDefaultModel();
 	Scanner sc = new Scanner(System.in);
 		  
      public  Model create() {
     	 
+    	 //initializing variables
     	 String _name = ""; 
     	 String _email = ""; 	 
-    	  
+    	 
+    	 //read name & email
     	 System.out.println("Name:");
     	 _name = sc.nextLine();
-    	 
+    	  	 
     	 System.out.println("E-mail:");
     	 _email = sc.nextLine();
     	 
+    	 //create a resource by setting an URI and add triples (subject - name - name; subject - email - mailto:mail)
 	     Resource _person= ResourceFactory.createResource("http://example.org/" + _name);
 	     _model.add (_person, RDF.type, FOAF.Person);
 	     _model.add (_person, FOAF.name, _name);
 	     _model.add (_person, FOAF.mbox, ResourceFactory.createResource("mailto:" + _email));
 	     
+	     //returns the model to the main class
 	     return _model;
 	     
      }
+      
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 
@@ -6,14 +8,39 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//initialize RDF Model, Input Scanner, and CRUD Classes
 		Model _mainModel = ModelFactory.createDefaultModel();
-		
+		Scanner sc = new Scanner(System.in);
 		Create _createUser = new Create();
 		
-		_mainModel = _createUser.create();
+		//initialize variables
+		int option = 0;
 		
-		_mainModel.write(System.out, "RDF/XML");
+		//main menu loop
+		do {
+			
+			//main menu and read option
+			System.out.println("Menu: ");
+			System.out.println("1 - Create User");
+			System.out.println("2 - Read Users");
+			System.out.println("0 - Exit");
+			System.out.println("Select an option: ");
+			option = sc.nextInt();
+			
+			//create user
+			if(option == 1) {
+				_mainModel = _createUser.create();
+			}
+			
+			//print RDF/XML
+			if(option == 2) {
+				_mainModel.getResource("http://example.org/qwe");
+				_mainModel.write(System.out, "RDF/XML");
+			}
+			
+		}while(option != 0);
 		
+		sc.close();
 		
 	}
 
